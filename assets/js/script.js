5612adf130224b71933fdbebb0339c594958213a
solaymanWeb <solaymanweb27@gmail.com>
2023-11-25 09:19:44 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	
+        $('.slider-wrapper').owlCarousel({
+            loop:true,
+			margin:0,
+			nav:true,
+			animateOut: 'fadeOut',
+    		animateIn: 'fadeIn',
+    		active: true,
+			smartSpeed: 1000,
+			autoplay: 6000,
+            navText: [ '<i class="flaticon-left"></i>', '<i class="flaticon-right-arrow"></i>' ],
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                800:{
+                    items:1
+                },
+                1024:{
+                    items:1
+                }
+            }
+        });
+    
+
+});
